f5aad20d5bb11b30335e517a7b0a8a173171b03d
Brett Stewart <brettstewart@users.noreply.github.com>
2024-09-12 11:01:49 -0500

    feat(lspconfig): enable eslint

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,7 +1,7 @@
 -- LSP Package Manager - https://github.com/williamboman/mason.nvim
 -- LSP Mason + Config Bridge - https://github.com/williamboman/mason-lspconfig.nvim
 -- LSP Client Config - https://github.com/neovim/nvim-lspconfig
-return { 
+return {
   {
     "williamboman/mason.nvim",
     config = function()
@@ -13,7 +13,7 @@ return {
     config = function()
       -- LSP Languages - https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "gopls" }
+        ensure_installed = { "lua_ls", "gopls", "eslint" }
       })
     end
   },
@@ -23,6 +23,7 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.gopls.setup({})
+      lspconfig.eslint.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
